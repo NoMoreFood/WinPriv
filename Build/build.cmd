c16c414f54891900c3eb9a30ae4edcead4277b89
Bryan Berns <berns@uwalumni.com>
2026-04-06 21:29:09 -0400

    Code Signing

--- Build/build.cmd
+++ Build/build.cmd
@@ -3,11 +3,6 @@ TITLE Building WinPriv...
 CLS
 SET PATH=%WINDIR%\system32;%WINDIR%\system32\WindowsPowerShell\v1.0
 
-:: cert info to use for signing
-SET TSAURL=http://time.certum.pl/
-SET LIBNAME=Repacls
-SET LIBURL=https://github.com/NoMoreFood/Repacls
-
 :: do cleanup
 RD /S /Q "%~dp0.vs" >NUL 2>&1
 RD /S /Q "%~dp0x86\Temp" >NUL 2>&1
@@ -22,19 +17,8 @@ FORFILES /S /P "%~dp0." /M "*.bsc" /C "CMD /C DEL /Q @path" >NUL 2>&1
 FORFILES /S /P "%~dp0." /M "*.exp" /C "CMD /C DEL /Q @path" >NUL 2>&1
 FORFILES /S /P "%~dp0." /M "*.last*" /C "CMD /C DEL /Q @path" >NUL 2>&1
 
-:: determine 32-bit program files directory
-IF DEFINED ProgramFiles SET PX86=%ProgramFiles%
-IF DEFINED ProgramFiles(x86) SET PX86=%ProgramFiles(x86)%
-
-:: setup commands and paths
-SET POWERSHELL=POWERSHELL.EXE -NoProfile -NonInteractive -NoLogo
-FOR /F "USEBACKQ DELIMS=" %%X IN (`DIR /OD /B /S "%PX86%\Windows Kits\10\SIGNTOOL.exe" ^| FINDSTR x64`) DO SET SIGNTOOL="%%~X"
-
-:: sign the main executables
-SET BINDIR=%~dp0
-%SIGNTOOL% sign /fd sha256 /tr %TSAURL% /td sha256 /d %LIBNAME% /du %LIBURL% "%BINDIR%\x86\*.exe" "%BINDIR%\x64\*.exe"
-
 :: zip up executatables
+SET BINDIR=%~dp0
 PUSHD "%BINDIR%"
 %POWERSHELL% -Command "Compress-Archive -LiteralPath @('x86','x64') -DestinationPath '%BINDIR%\WinPriv.zip'"
 POPD
