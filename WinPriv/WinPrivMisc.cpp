e14d254af7863a30b4b9708de573e4a95dccfc18
Bryan Berns <berns@uwalumni.com>
2026-04-16 17:55:49 -0400

    Correct Help Information

--- WinPriv/WinPrivMisc.cpp
+++ WinPriv/WinPrivMisc.cpp
@@ -79,7 +79,7 @@ std::wstring GetWinPrivHelp()
 	return std::wstring(PROJECT_NAME) +
 		LR"(.exe [optional switches] <Command To Execute>
 
-WinPriv is system administration utility that alters the runtime behavior of
+WinPriv is a system administration utility that alters the runtime behavior of
 the specified process and its child processes. It does this by loading a
 supplemental library into memory to intercept and alter the behavior of
 common low-level functions such as registry and file system operations.
@@ -87,15 +87,15 @@ common low-level functions such as registry and file system operations.
 WinPriv can be used for a variety of purposes including testing security
 settings without altering system-wide policy, implementing security-related
 workarounds on a per-process basis instead of altering system-wide policy, and
-taking advantageous of system privileges to perform file system auditing and
+taking advantage of system privileges to perform file system auditing and
 reconfiguration.
 
-WinPriv comes in normal version (WinPriv) and a console version (WinPrivCmd).
+WinPriv comes in a normal version (WinPriv) and a console version (WinPrivCmd).
 The behavior of the subprocess is the same regardless of which version is used.
-These versions are provided in case the target program is a console program
+These versions are provided in case the target program is a console program,
 in which case you will only be able to get its screen output if you use
-WinPrivCmd. Similarly, you may not wish to see the console windows when
-targeting a non-console program in which case it may be advantageous to use
+WinPrivCmd. Similarly, you may not wish to see the console window when
+targeting a non-console program, in which case it may be advantageous to use
 WinPriv.
 
 Optional Switches
@@ -105,23 +105,23 @@ Optional Switches
 
    Specifies a registry value to override. Instead of returning the true
    registry value for the specified key path and value name, the value
-   the value specified in this switch is returned.
+   specified in this switch is returned.
 
    Examples:
 
-      /RegOverride HKCU\Software\Demo Enabled REG_DWORD 1
-      /RegOverride HKLM\Software\Demo UserName REG_SZ "James Bond"
+	  /RegOverride HKCU\Software\Demo Enabled REG_DWORD 1
+	  /RegOverride HKLM\Software\Demo UserName REG_SZ "James Bond"
 
 /RegBlock <Registry Key Path>
 
-   Specified a registry key under which all values will be reported as
+   Specifies a registry key under which all values will be reported as
    non-existent. When the application requests a particular value in the
    specified key or one of its subkeys, it will be reported as not found
-   regardless as to whether it actually exists in the registry.
+   regardless of whether it actually exists in the registry.
 
    Examples:
 
-      /RegBlock HKCU\Software\Demo
+	  /RegBlock HKCU\Software\Demo
 
 /MacOverride <MAC Address>
 
@@ -132,53 +132,54 @@ Optional Switches
 
    Examples:
 
-      /MacOverride 00-11-22-33-44-66
+	  /MacOverride 00-11-22-33-44-66
 
 /HostOverride <Target HostName> <Replacement HostName>
 
    Specifies that any request to obtain the IP address for the specified target
    will instead receive the specified replacement IP address. This is done by
-   intercepting calls to WSALookupServiceNext() which nearly all address
-   lookups ultimately occur. Be aware due to special security protections,
-   this will not work for Internet Explorer and programs that use Internet
-   Explorer libraries but should work for most other processes.
+   intercepting calls to WSALookupServiceNext(), through which nearly all
+   address lookups ultimately occur. Be aware that due to special security
+   protections, this will not work for Internet Explorer and programs that use
+   Internet Explorer libraries, but should work for most other processes.
 
    Examples:
 
-      /HostOverride google.com yahoo.com
-      /HostOverride google.com 127.0.0.1
+	  /HostOverride google.com yahoo.com
+	  /HostOverride google.com 127.0.0.1
 
 /FipsOn & /FipsOff
 
-   This option will turn cause the system to report that Federal Information
-   Processing System enforcement is turned on or off, regardless of its current
-   setting on the system. This operation is a convenience option that actually
+   This option will cause the system to report that Federal Information
+   Processing Standard enforcement is turned on or off, regardless of its
+   current setting on the system. This is a convenience option that actually
    uses the /RegOverride functionality on the FIPS-related registry key.
 
 /PolicyBlock
 
    This option will cause all registry queries to HKCU\Software\Policies and
-   HKLM\Software\Policies. This option is convenience option that actually uses
-   the /RegBlock functionality.
+   HKLM\Software\Policies to be blocked. This is a convenience option that
+   actually uses the /RegBlock functionality.
 
 /BypassFileSecurity
 
    This option causes the target process to enable the backup and restore
-   privileges and alters the way the program access files to take advantage of
-   this extra privileges. When these privileges are enabled, all access
+   privileges and alters the way the program accesses files to take advantage
+   of these extra privileges. When these privileges are enabled, all access
    control lists on the file system are ignored. This allows an administrator
    to inspect and alter files without changing permissions or taking ownership.
 
    Effective uses of this option include using command line utilities like
-   icacls.exe to inspect/alter permissions. Using this with cmd.exe or
-   powershell.exe also provide a mean to interact with secured areas.
+   icacls.exe to inspect or alter permissions. Using this with cmd.exe or
+   powershell.exe also provides a means to interact with secured areas.
 
    Examples:
 
    Access detailed permissions under 'C:\System Volume Information':
    WinPrivCmd.exe /BypassFileSecurity icacls.exe
-      "C:\System Volume Information" /T
-
+	  "C:\System Volume Information" /T
+)"
+		LR"(
 /BreakRemoteLocks
 
    This option attempts to break remote file locks if a file cannot be accessed
@@ -198,22 +199,22 @@ Optional Switches
 /AdminImpersonate
 
    This option causes any local administrator check using IsUserAnAdmin() or
-   CheckTokenMembership() to unconditionally succeed regardless if the user is
-   actually a member of the local administrator group.
+   CheckTokenMembership() to unconditionally succeed regardless of whether the
+   user is actually a member of the local administrator group.
 
 /ServerEdition
 
    This option causes the most common operating system version information
-   functions to indicate the that the system is running a server edition of
-   the operating system.
+   functions to indicate that the system is running a server edition of the
+   operating system.
 
 /RecordCrypto <Directory>
 
-   This option records the data being inputted to common Windows encryption
-   functions and the data being outputted from common Windows decryption
+   This option records the data being input to common Windows encryption
+   functions and the data being output from common Windows decryption
    functions. A separate file will be created for each operation in the
    specified directory. If 'SHOW' is specified instead of a directory path,
-   information is outputted to the console and message boxes, depending of the
+   information is output to the console or message boxes, depending on the
    type of application.
 
 /SqlConnectShow
@@ -223,26 +224,27 @@ Optional Switches
 
 /SqlConnectSearchReplace <SearchString> <ReplaceString>
 
-   This option performs a search / replace on an ODBC connection string prior
-   to passing it to the connection Open() function.  The search string is
+   This option performs a search and replace on an ODBC connection string prior
+   to passing it to the connection Open() function. The search string is
    parsed as a regular expression.
 
    Examples:
 
    WinPrivCmd.exe /SqlConnectSearchReplace
-      Provider=SQLOLEDB Provider=SQLNCLI11 LegacyApplication.exe
+	  Provider=SQLOLEDB Provider=SQLNCLI11 LegacyApplication.exe
 
 /KillProcess <ProcessName>
 
-   Kill the process with specified name prior to running the target. This is
-   useful if the target has logic to prevent multiple execution and needs to
-   to be terminated before the effects of a WinPriv session can be effective.
+   Kills the process with the specified name prior to running the target. This
+   is useful if the target has logic to prevent multiple instances from running
+   and needs to be terminated before the effects of a WinPriv session can be
+   effective.
 
 /ExtractLibrary
 
    This option extracts the embedded 32-bit and 64-bit libraries to the
    directory where WinPriv is running. These are normally dynamically extracted
-   to the users temporary directory. If WinPriv finds these libraries in the
+   to the user's temporary directory. If WinPriv finds these libraries in the
    directory where it is running, it will use those instead of writing them
    to the temporary directory.
 
@@ -253,8 +255,8 @@ Optional Switches
 
 /UseShellExecute
 
-   This option will launch the target process with ShellExecute() function
-   instead of CreateProcess().  This can be useful if launching an application
+   This option will launch the target process with the ShellExecute() function
+   instead of CreateProcess(). This can be useful if launching an application
    that is registered on the system but not in the system path.
 
 /MeasureTime
@@ -264,14 +266,14 @@ Optional Switches
 
 /ListPrivileges
 
-   This option displays of list of available privileges and permissions.
+   This option displays a list of available privileges and permissions.
 
 /RunAsConsoleUser
 
-   Runs the specified program as the user that is logged into the console. 
-   If no user is logged into the console, the first active remote user 
-   session is used.  This can be useful when WinPriv is running as under
-   a system context such as a scheduled task or system management agent.
+   Runs the specified program as the user that is logged into the console.
+   If no user is logged into the console, the first active remote user
+   session is used. This can be useful when WinPriv is running under a system
+   context such as a scheduled task or system management agent.
 
 /RunAsConsoleUserNoWait
 
@@ -283,13 +285,13 @@ Optional Switches
 
    Runs the specified program as the specified user. The user must be logged
    into the system at the console or remotely. This can be useful when WinPriv
-   is running as under a system context such as a scheduled task or
-   system management agent.
+   is running under a system context such as a scheduled task or system
+   management agent.
 
 Other Notes & Limitations
 =========================
 - Multiple switches can be specified in a single command. For example, one
-  can use multiple /RegBlock and /RegOverride commands to block and override
-  the specified set of registry key and values on a specified target program.
+  can use multiple /RegBlock and /RegOverride switches to block and override
+  a specified set of registry keys and values for a target program.
 )";
 }
